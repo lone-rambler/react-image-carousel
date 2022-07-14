d603796bdb77fc5283d318c0aa8a6a0ff70a374c
lone-rambler <maksymilian.wiese99@gmail.com>
2022-07-14 14:31:28 +0200

    fade in/out done

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -1,13 +1,56 @@
-import React, { useState } from 'react'
-import { StyledImg } from '../styles/Styles.js'
+import React, { useState, createRef } from 'react'
+import {
+	StyledImg,
+	LeftArrowStyle,
+	RightArrowStyle,
+	ActiveStyle,
+	InactiveStyle,
+} from '../styles/Styles.js'
+import { ReactComponent as Arrow } from '../images/arrow.svg'
+import ImgsData from './ImgsData.json'
 
 export default function Carousel(props) {
-	const [currentId, setCurrentId] = useState(0)
+	const [active, setActive] = useState(0)
+	const length = ImgsData.images.length
+
+	if (length <= 0) {
+		return <div>brak danych</div>
+	}
+
+	const slideLeft = () => {
+		if (active === 0) {
+			setActive(length - 1)
+		} else {
+			setActive(active - 1)
+		}
+		console.log(active)
+	}
+
+	const slideRight = () => {
+		if (active === length - 1) {
+			setActive(0)
+		} else {
+			setActive(active + 1)
+		}
+		console.log(active)
+	}
 
 	return (
 		<>
-			<StyledImg id='0' src={require('../imgs/1.jpg')} />
-			<StyledImg id='1' src={require('../imgs/2.jpg')} />
+			<Arrow style={LeftArrowStyle} onClick={slideLeft}></Arrow>
+			<Arrow style={RightArrowStyle} onClick={slideRight}></Arrow>
+
+			{ImgsData &&
+				ImgsData.images.map((img) => {
+					return (
+						<StyledImg
+							src={`${img.src}`}
+							alt={img.alt}
+							key={img.id}
+							style={active === img.id ? ActiveStyle : InactiveStyle}
+						/>
+					)
+				})}
 		</>
 	)
 }
--- src/styles/Styles.js
+++ src/styles/Styles.js
@@ -8,11 +8,45 @@ export const GlobalStyles = createGlobalStyle`
         margin: 0;
 
         font-family: 'Cormorant SC', serif;
+
+		user-select: none;
+		
+		overflow-x: hidden;
     }
 `
 
 export const StyledImg = styled.img`
 	width: 100%;
 	height: 100vh;
+	position: absolute;
 	object-fit: cover;
+	transition: all 500ms ease-in;
 `
+
+export const LeftArrowStyle = {
+	height: 50,
+	width: 50,
+	position: 'absolute',
+	top: '45vh',
+	left: '1rem',
+	cursor: 'pointer',
+	zIndex: 999,
+}
+
+export const RightArrowStyle = {
+	height: 50,
+	width: 50,
+	position: 'absolute',
+	top: '45vh',
+	right: '1rem',
+	transform: 'rotate(0.5turn)',
+	cursor: 'pointer',
+	zIndex: 999,
+}
+
+export const ActiveStyle = {}
+
+export const InactiveStyle = {
+	opacity: 0,
+	zIndex: 0,
+}
